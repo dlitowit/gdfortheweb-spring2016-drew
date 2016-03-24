83e8ef0bbbdb4d3c7a4352ef5b9a5424b82543ee
dlitowit <dlitowit@risd.edu>
2016-03-24 17:34:32 -0400

    pink

--- project3/main.js
+++ project3/main.js
@@ -1,38 +1,41 @@
 // a variable
-var name = "Drew";
-var zip = 02906;
 var colors = ["#ffff00", "orange", "#e5e500", "#ffc04c","yellow"];
-var words = ["coming to", "RISD", "visiting", "designer", "yay!", "she's coming!!", "form", "CIT 103"];
+var words = ["coming", "RISD", "visiting", "designer", "yay!", "2016", "March", "CIT 103", "10-13", "Keetra", "Dean", "Dixon", "is", "a", "Designer", ",", "Director", "and", "Artist", ".", "Her", "hybrid", "design", "background", "and", "expertise", "in", "graphic" ,"design", "often", "leads", "her", "work", "towards", "speculative", "terrain", "leveraging", "emergent", "technologies", "the", "shortcomings", "of", "ubiquitous", "creative", "tools"];
 
 // functions
-function sayHello() {
-	var greeting = "Hello " + name;
-	console.log(greeting);
-}
 
-$(".keetra").click(function() {
-	$("#text").toggle();
+$(".keetra").on("click", function() {
+	// $(".name").text("A Lecture and Workshop");
+
+	$(".lecture").toggle();
 });
+
+
+
+
+// $("body").on("click", function(){
+// 	$(".keetra").html ("A Lecture and a Workshop");
+// });
+// $("body").on("mouseup", function(){
+// 	$(".keetra").html("Keetra Dixon")
+// });
 
 
 $(".keetra").hover(function() {
 	$(".confetti").each(function() {
+		
 		var randNumber = Math.random();
-		$(this).html("visiting designer");
+		$(this).css ("transform", "rotate(" + (randNumber/10) + "deg)");
 		$(this).toggleClass("hovered")
+		
 		var randcolor = colors[Math.floor(Math.random() * colors.length)];
 		$(this).css("color", randcolor);
 		$(this).css("font-size", "." + (randNumber/10));
+		
 		var wordchange = words[Math.floor(Math.random() * words.length)];
 		$(this).html(wordchange);
-
 	});
 	});
-
-
-	//$(this).html("DOPE")
-	//$(this).removeClass("hovered")
-	//$(this).html(" ");
 	
 
 $(document).on('mousemove', function(e){
@@ -41,5 +44,3 @@ $(document).on('mousemove', function(e){
        top:   e.pageY
     });
 });
-// calling a function
-sayHello();	
