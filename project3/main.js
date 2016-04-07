06f3dfd595dca2dfeed6da9dc85f69c85ff3abd0
dlitowit <dlitowit@risd.edu>
2016-04-07 11:32:20 -0400

    project3

--- project3/main.js
+++ project3/main.js
@@ -1,7 +1,7 @@
 // a variable
 var colors = ["#F1FE00", "orange", "#e5e500", "#00FF38","yellow","#FF4900"];
 var words = ["coming", "RISD", "stunning", "amazing", "visiting", "designer", "yay!", "2016", "March", "CIT 103", "10-13", "Keetra", "Dean", "Dixon", "is", "a", "Designer", ",", "Director", "and", "Artist", ".", "Her", "hybrid", "design", "background", "and", "expertise", "in", "graphic" ,"design", "often", "leads", "her", "work", "towards", "speculative", "terrain", "leveraging", "emergent", "technologies", "the", "shortcomings", "of", "ubiquitous", "creative", "tools"];
-
+var typeface = ["arial", "terminal-grotesque_open","Times","Comic Sans","hobo", "impact", "Verdana", "courier new"]
 // functions
 
 $(".keetra").on("click", function() {
@@ -21,6 +21,29 @@ $(".keetra").on("click", function() {
 // 	$(".keetra").html("Keetra Dixon")
 // });
 
+$(".keetra").mousemove(function() {
+		var typechange = typeface[Math.floor(Math.random() * typeface.length)];
+		$(this).css("font-family", typechange);
+
+		});
+$(document).ready(function() {
+
+$(".keetra").mousemove(function(){
+        var div = $(this); 
+        var chars = div.text().split('');
+        div.html('');     
+        for(var i=0; i<chars.length; i++) {
+            var typechange = Math.floor(Math.random() * typeface.length);
+            var span = $('<span>' + chars[i] + '</span>').css("font-family", typechange)
+            div.append(span);
+        }
+
+    }, function() {
+        $(this).find('span').css("font-family");
+    });
+
+});
+
 
 $(".keetra").hover(function() {
 	$(".confetti").each(function() {
@@ -35,6 +58,10 @@ $(".keetra").hover(function() {
 		
 		var wordchange = words[Math.floor(Math.random() * words.length)];
 		$(this).html(wordchange);
+
+
+
+
 	});
 	});
 	
